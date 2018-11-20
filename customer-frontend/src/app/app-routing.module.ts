fb6ebdbf9948daf19a7f3c701aa41029ecc548ff
John Smith <badass.ninja.hacker@example.com>
2018-11-20 11:51:30 +0000

    Added T&Cs

--- customer-frontend/src/app/app-routing.module.ts
+++ customer-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'testingroute', component: RegisterComponent },
+    { path: 'testingroute', component: TermsAndConditionsComponent },
 ];
 
 @NgModule({
--- customer-frontend/src/app/app.module.ts
+++ customer-frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { RegisterComponent } from './register/register.component';
     HomeComponent,
     ProductCardComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    TermsAndConditionsComponent
   ],
   imports: [
     BrowserModule,
