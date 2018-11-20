ff172295c42377867df9c6d558fafe64494abd18
John Smith <badass.ninja.hacker@example.com>
2018-11-20 10:04:29 +0000

    Added Boostrap 4 card for product card styling

--- customer-frontend/src/app/app-routing.module.ts
+++ customer-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductCardComponent from './product-card/product-card.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: '/card', component: ProductCardComponent },
+    { path: 'card', component: ProductCardComponent },
 ];
 
 @NgModule({
